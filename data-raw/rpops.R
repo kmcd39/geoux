574a763a2f015f4697a9b4c7d7c96c721e7a3831
Kira McDonald <kira.mcd.99@gmail.com>
2021-09-22 21:55:05 -0400

    adding rpops which has populations for all czs/cbsas to exported data

---
+++ data-raw/rpops.R
@@ -0,0 +1,52 @@
+# boilderplate ---------------------------------------------------------------------------
+rm(list = ls())
+require(tidyverse)
+# option setting
+options(tigris_use_cache = TRUE)
+# get all regions with 250+ pop -------------------------------------------
+
+# will move this to geox soon...
+
+pops <-
+  map_dfr(geox::rx$countyfp
+          ,~tidycensus::get_acs(
+            geography = 'county'
+            ,state = substr(.x, 1,2)
+            ,county = substr(.x, 3,5)
+            ,year =2019
+            ,survey = "acs5"
+            ,variable = 'B01001_001'
+          ))
+
+pops <- pops %>%
+  rename_with( ~tolower(.x)) %>%
+  select(geoid, pop = estimate)
+pops <- pops %>%
+  left_join(geox::rx
+            ,by=c('geoid' = 'countyfp'))
+cz.pops <- pops %>%
+  group_by(cz) %>%
+  summarise(pop = sum(pop)) %>%
+  ungroup() %>%
+  filter(!is.na(cz))
+
+cbsa.pops <-  pops %>%
+  group_by(cbsa) %>%
+  summarise(pop = sum(pop)) %>%
+  ungroup() %>%
+  filter(!is.na(cbsa))
+
+rpops <- rbind(
+  geox::region.reorg(cz.pops, 'cz'),
+  geox::region.reorg(cbsa.pops, 'cbsa')
+  )
+
+rpops %>%
+  filter(pop > 250e3) %>%
+  map( ~ sum(is.na(.)))
+
+
+
+# write ------------------------------------------------------------------------
+
+usethis::use_data(rpops)
