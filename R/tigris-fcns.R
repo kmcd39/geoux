0d86c5e4fa7d076d2f35528e5f4ec43f087c65c4
Kira McDonald <kira.mcd.99@gmail.com>
2021-11-19 14:25:41 -0500

    new improved query fcns

--- R/tigris-fcns.R
+++ R/tigris-fcns.R
@@ -10,23 +10,23 @@
 
 #' county.subset
 #'
-#' Given an `sf` object, get all counties that intersect. The spatial filter will
-#' adapt based on input, but can be thrown off when `x` isn't coterminous with
-#' counties. In this case the function `xwalks::get.spatial.overlap` can be helpful.
+#' Given an `sf` object or bbox, get all counties that fall within polygon or bbox.
 #'
-#' @param x `sf` or `bbox`
+#' @inheritParams flexible.spatial.filter
 #' @param cos counties sf object. If none supplied, they are retrieved using
-#'   `tigris` library
-#' @inheritDotParams flexible.spatial.filter
+#'   `tigris`/census api
+#' @param ... passed onto `tigris::counties`
+#'
 #'
 #' @export county.subset
-county.subset <- function(x, cos = NULL, year = 2019, ...) {
+county.subset <- function(x, cos = NULL,
+                          ...) {
 
   if(is.null(cos))
-    cos <- tigris::counties(year = year) %>%
+    cos <- tigris::counties(...) %>%
       rename_with(tolower)
 
-  cos <- flexible.spatial.filter(x, cos, ...)
+  cos <- flexible.spatial.filter(x, cos)
 
   return(cos)
 }
@@ -39,66 +39,135 @@ county.subset <- function(x, cos = NULL, year = 2019, ...) {
 #' Note that they may still be trimmed to match supplied area, if `x` isn't
 #' coterminous with counties.
 #'
-#' @param x sf object to get neighborhoods over
-#' @param countyfps Alternative to x, countyfp (as 5-char characters) to get nhoods for.
+#' @inheritParams flexible.spatial.filter
+#' @param .countyfps Alternative to x, countyfp (as 5-char characters) to get nhoods
+#'   for.
 #' @param query.fcn tigris query fcn: `tracts` or `block_groups`
 #' @param ... passed onto `query.fcn`
 #'
 #' @export tracts.from.sf
-tracts.from.sf <- function(x = NULL, countyfps = NULL,
+tracts.from.sf <- function(x = NULL, .countyfps = NULL,
                            query.fcn = tigris::tracts,
                            ...) {
 
-  if(is.null(countyfps)) {
+  if(is.null(.countyfps)) {
     cos <- county.subset(x)
-    countyfps <- cos$geoid
+    .countyfps <- cos$geoid
   }
 
   # download
   .params <- list(...)
 
-  .nhds <- map_dfr(countyfps,
+  nhds <- map_dfr(.countyfps,
                    ~do.call(query.fcn
                             , c(list( substr(.x, 1, 2)
                                      ,substr(.x, 3, 5))
                                 , .params))) %>%
     rename_with(tolower)
 
+  if(!is.null(x))
+    nhds <- flexible.spatial.filter(x, nhds)
 
-  return(.nhds)
+
+  return(nhds)
 }
 
+
+#' places.wrapper
+#'
+#' Gets parks in a state, based on census bureau data. Can query from sf object x or
+#' state fp codes.
+#'
+#' @inheritParams flexible.spatial.filter
+#' @param .statefps statefps to get areas for
+#' @param ... passed onto `tigris::places`
+#'
+#' @export places.wrapper
+places.wrapper <- function(x = NULL, .statefps = NULL,
+                           ...) {
+
+  if(is.null(.statefps)) {
+    cos <- county.subset(x)
+    .countyfps <- cos$geoid
+    .statefps <- substr(.countyfps, 1, 2)
+  }
+
+  plcs <- map_dfr(.statefps,
+                  ~tigris::places( state = .
+                                   , ...
+                  ),
+                  ...) %>%
+    rename_with(tolower)
+
+  if(!is.null(x))
+    plcs <- flexible.spatial.filter(x, plcs)
+
+  return(plcs)
+
+}
+
+
+#' parks.wrapper
+#'
+#' Gets parks in a state, based on census bureau data. Can query from sf object x or
+#' state fp codes.
+#'
+#' @inheritParams parks.wrapper
+#'
+#' @export parks.wrapper
+parks.wrapper <- function(x = NULL, .statefps = NULL,
+                          ...) {
+
+  if(is.null(.statefps)) {
+    cos <- county.subset(x)
+    .countyfps <- cos$geoid
+    .statefps <- substr(.countyfps, 1, 2)
+  }
+
+  parks <- map_dfr(.statefps,
+                   ~tigris::landmarks(.x
+                                      , type = "area"
+                                      , ...)
+                   , ...) %>%
+    rename_with(tolower)
+
+  parks <- parks[grepl('Park|Cmtry', parks$fullname),]
+
+  if(!is.null(x))
+    parks <- flexible.spatial.filter(x, parks)
+
+  return(parks)
+}
 
 
 #' water.wrapper
 #'
 #' Gets water areas based on supplied countyfp codes and/or other spatial area.
 #'
-#' @param countyfps 5-character state/county fp codes. Retrieved using
-#'   `county.subset` and supplied `x` argument if null.
-#' @param x `sf` or `bbox` to get overlapping water areas for. Passed onto
-#'   `county.subset` if no county fps are supplied. Also used to spatially subset
-#'   water areas.
+#'
+#' @inheritParams tracts.from.sf
 #' @param size.min Minimum size in m^2, after internal boundaries are resolved (if a
 #'   water area is represented by multiple contiguous polygons)
 #'
 #' @return water areas for region.
 #'
 #' @export water.wrapper
-water.wrapper <- function(countyfps = NULL, x = NULL, size.min = 5e6, ...) {
+water.wrapper <- function(x = NULL, .countyfps = NULL,
+                          size.min = 5e6, ...) {
 
-  if(is.null(countyfps)) {
-    cos <- county.subset(x, ...)
-    countyfps <- cos$geoid
+  if(is.null(.countyfps)) {
+    cos <- county.subset(x)
+    .countyfps <- cos$geoid
   }
 
   # download water
-  water <- map_dfr(countyfps,
+  water <- map_dfr(.countyfps,
                    ~tigris::area_water(state =
                                          substr(., 1, 2),
                                        county =
-                                         substr(., 3, 5))
-  )
+                                         substr(., 3, 5),
+                                       ...)
+                   , ...)
 
   # union and explode water
   water <- st_union(water) %>% st_cast("POLYGON") %>% st_sf()
@@ -107,67 +176,11 @@ water.wrapper <- function(countyfps = NULL, x = NULL, size.min = 5e6, ...) {
   water <- water %>% filter(as.numeric(st_area(.$geometry)) > size.min )
 
   if(!is.null(x)) {
-    water <- transform.and.crop(water, x)
+    water <- flexible.spatial.filter(x, water)
   }
 
   return(water)
 }
-
-
-#' parks.wrapper
-#'
-#' @param x object to derive overlapping statefps from, if statefps is left as null. Aiddtional
-#' @param statefps statefps to get parks for
-#'
-#' @export parks.wrapper
-parks.wrapper <- function(x = NULL, statefps = NULL, ...) {
-
-  if(!is.null(statefps)) {
-    cos <- county.subset(x, ...)
-    statefps <- cos$statefp
-  }
-
-  parks <- map_dfr(statefps,
-                   ~tigris::landmarks(.x, type = "area")
-  )
-
-  colnames(parks) <- tolower(colnames(parks))
-
-  parks <- parks[grepl('Park|Cmtry', parks$fullname),]
-
-  if(!is.null(x))
-    parks <- transform.and.crop(parks, x)
-
-  return(parks)
-}
-
-
-#' places.wrapper
-#'
-#' @param x object to derive overlapping statefps from, if statefps is left as null. Aiddtional
-#' @param countyfps countyfps to get parks for
-#'
-#' @export places.wrapper
-places.wrapper <- function(.countyfps = NULL, x = NULL, year = 2019,
-                           ...) {
-
-  if(is.null(.countyfps)) {
-    cos <- county.subset(x, ...)
-    .countyfps <- cos$geoid
-  }
-  .statefps <- substr(.countyfps, 1, 2)
-
-  plcs <- map_dfr(.statefps,
-                  ~tigris::places(.x, year = year),
-                  ...)
-
-  if(!is.null(x))
-    plcs <- transform.and.crop(plcs, x)
-
-  return(plcs)
-
-}
-
 
 # helpers ----------------------------------------------------------------------
 
@@ -184,48 +197,30 @@ places.wrapper <- function(.countyfps = NULL, x = NULL, year = 2019,
 #' Currently this doesn't work with non-coterminous polys. In this case, use
 #' `xwalks::get.spatial.overlap`, and filter by degree of overlap.
 #'
-#' @x `sf` object spanning area you want to filter `polys` to
+#' @param x `sf` or `bbox` object spanning area you want to filter polys to. Will use
+#'   get intersection if sf object or crop if bbox.
 #' @param polys polygons to get over area `x`.
 #' @param subset.approach spatial filter approach; one of "intersects" or "crop".
 #'   Intersection or cropping to bbox.
 #'
-flexible.spatial.filter <- function(x, polys,
-                                    subset.approach = c('intersects', 'crop')) {
+flexible.spatial.filter <- function(x, polys) {
 
   require(sf)
 
   polys <- st_transform(polys, st_crs(x))
 
   # use given subset approach. Use crop if x is a bbox
-  if(subset.approach[1] == "intersects" &
-     !"bbox" %in% class(x)) {
+  if(!"bbox" %in% class(x)) {
 
-    polys <-  st_point_on_surface(polys)
-    sbgp <- st_intersects(polys, x)
-
+    pts <-  st_point_on_surface(polys)
+    sbgp <- st_intersects(pts, x)
     polys <- polys[lengths(sbgp) > 0, ]
 
-  } else if(subset.approach[1] == "crop" |
-            "bbox" %in% class(x))
-
+  } else if("bbox" %in% class(x))
     polys <- st_crop(polys, x)
 
   return(polys)
 }
 
-
-#' transform.and.crop
-#'
-#' Steps common to many of the wrappers if an sf object is supplied to one of the
-#' tigris wrappers. Subsets and crops the input.
-#'
-#' @param basis.layer Layer or bbox to match with crs and crop
-#'
-transform.and.crop <- function(new.layer, basis.layer) {
-
-  new.layer %>%
-    st_transform(st_crs(basis.layer)) %>%
-    st_crop(basis.layer)
-}
 
 
--- tests/testthat/test-query-fcns.R
+++ tests/testthat/test-query-fcns.R
@@ -38,20 +38,80 @@ czsf <- geox::build.CZs('20100')
 
 # cos --------------------------------------------------------------------------
 
-test
 cofps <- geox::x2cos(cz = '20100')
 cosf <- geox::county.subset(x = czsf)
+
+cofps
+head(cosf)
 
 testthat::test_that("spatial query counties", {
   testthat::expect_equal(sort(cofps)
                          , sort(cosf$geoid))
 })
 
+cosf[2] %>% plot()
 
 # tracts -----------------------------------------------------------------------
 
+
 ctsf <- geox::tracts.from.sf(x = czsf)
 bgsf <- geox::tracts.from.sf(x = czsf
-                             ,query.fcn = tigris::block_groups)
-ctsf['aland'] %>% plot()
-bgsf['aland'] %>% plot()
+                             ,query.fcn = tigris::block_groups
+                             ,cb = T
+                             ,year = 2018
+                             )
+ctsf['countyfp'] %>% plot()
+bgsf['countyfp'] %>% plot()
+
+# for just portland's county
+portlandco <- cosf %>% filter(grepl('Cumberland', name))
+pctsf <- geox::tracts.from.sf(x = portlandco)
+pbgsf <- geox::tracts.from.sf(x = portlandco
+                             ,query.fcn = tigris::block_groups
+                             ,cb = T
+                             ,year = 2018)
+
+pctsf['aland'] %>% plot()
+pbgsf['aland'] %>% plot()
+
+# bbox crop -- very cool :)
+pbbgsf <- geox::tracts.from.sf(x = st_bbox(portlandco)
+                              ,query.fcn = tigris::block_groups
+                              ,cb = T
+                              ,year = 2018)
+pbbgsf['aland'] %>% plot()
+
+# places -----------------------------------------------------------------------
+
+library(mapview)
+devtools::load_all()
+
+plcsf.r <- geox::places.wrapper(x = st_bbox(czsf)
+                              , year = 2020
+                              )
+
+plcsf.old <-  geox::places.wrapper(x = st_bbox(czsf)
+                                   , year = 2011
+                                   )
+
+
+plcsf.r['aland'] %>% plot()
+plcsf.old['aland'] %>% plot()
+
+czsf %>% mapview::mapview() + mapview::mapview(plcsf)
+
+
+# parks ------------------------------------------------------------------------
+
+parksf <- geox::parks.wrapper(x = portlandco
+                              , year = 2019)
+
+parksf
+
+parksfi <- geox::parks.wrapper(x = portlandco
+                              , year = 2019
+                              , subset.approach = 'intersects'
+                              )
+
+parksf['aland'] %>% plot()# + mapview(st_boundary(portlandco))
+parksfi['aland'] %>% plot()
