0c1af441931d69e4dd77e719b41c966ac8dc4519
Kira McDonald <kira.mcd.99@gmail.com>
2024-07-19 13:26:14 -0400

    uploading region/division xwalk

---
+++ data-raw/regionx.R
@@ -0,0 +1,45 @@
+library(tidyverse)
+library(sf)
+
+
+# pull regions, states and census divisions -------------------------------
+
+tigris::regions()
+regionx <- tigris::states(year = 2022) %>%
+  tibble() %>%
+  rename_with(tolower) %>%
+  select(region, division, statefp, stusps, state = name)
+
+regionx
+
+
+## pull regions/divs for names ---------------------------------------------
+
+divs <- tigris::divisions(year = 2022) %>%
+  tibble() %>%
+  rename_with(tolower)  %>%
+  select(div.geoid = geoid, division.name = name)
+
+regions <- tigris::regions(year = 2022) %>%
+  tibble() %>%
+  rename_with(tolower)  %>%
+  select(reg.geoid = geoid, region.name = name)
+
+regionx <- regionx %>%
+  left_join(divs
+            , by = c("division" = "div.geoid")
+            ) %>%
+  left_join(regions
+            , by = c("region" = "reg.geoid")
+            )
+
+regionx <- regionx %>%
+  select(matches("state"), matches("region"), matches("division"))
+
+# write ------------------------------------namespaceImportMethods()# write -------------------------------------------------------------------
+
+
+
+## code to prepare `regionx` dataset goes here
+
+usethis::use_data(regionx, overwrite = TRUE)
